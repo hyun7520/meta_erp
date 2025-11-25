c4a07771af2e96cc307baddc824e6bf53e14a4c9
HJ_Kim <ruby45342@gmail.com>
2025-11-25 16:21:09 +0900

    25.11.25 add role for login

--- src/main/java/com/meta/stock/order/controller/OrderController.java
+++ src/main/java/com/meta/stock/order/controller/OrderController.java
@@ -1,17 +1,35 @@
 package com.meta.stock.order.controller;
 
+import com.meta.stock.user.employees.dto.EmployeeGetDto;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OrderController {
     @GetMapping("/order")
-    public String goOrder() {
+    public String goOrder(HttpSession session) {
+        if (session.getAttribute("employee") == null) {
+            return "redirect:/login";
+        } else {
+            EmployeeGetDto employee = (EmployeeGetDto) session.getAttribute("employee");
+            if (employee.getDepartment().equals("생산") || employee.getRole().equals("사원")) {
+                return "redirect:/dash";
+            }
+        }
         return "order/order";
     }
 
     @GetMapping("/request")
-    public String goMaterialRequest() {
+    public String goMaterialRequest(HttpSession session) {
+        if (session.getAttribute("employee") == null) {
+            return "redirect:/login";
+        } else {
+            EmployeeGetDto employee = (EmployeeGetDto) session.getAttribute("employee");
+            if (employee.getDepartment().equals("생산") || employee.getRole().equals("사원")) {
+                return "redirect:/dash";
+            }
+        }
         return "order/material_request";
     }
 }
