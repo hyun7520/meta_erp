d649fde9b0527560708ceb87255ec853f092247c
HJ_Kim <ruby45342@gmail.com>
2025-11-28 15:23:36 +0900

    5차 배포 알림 오늘만보기 추가

--- src/main/resources/static/script/notification.js
+++ src/main/resources/static/script/notification.js
@@ -1,11 +1,11 @@
 window.onload = checkAlert;
 function checkAlert() {
-    // const offset = 1000 * 60 * 60 * 9;
-    // const today = new Date(new Date().getTime() + offset)
-    //     .toISOString()
-    //     .split("T")[0];
-    // const hiddenDate = localStorage.getItem("hideAlertDate");
-    // if (hiddenDate === today) return; // 오늘 안 보기 체크했으면 종료
+    const offset = 1000 * 60 * 60 * 9;
+    const today = new Date(new Date().getTime() + offset)
+        .toISOString()
+        .split("T")[0];
+    const hiddenDate = localStorage.getItem("hideAlertDate");
+    if (hiddenDate === today) return; // 오늘 안 보기 체크했으면 종료
     // 실제로는 fetch('forecast_alert_v2.json') 사용
     // 여기서는 테스트를 위해 가짜 데이터를 사용합니다.
 
@@ -34,10 +34,10 @@ function checkAlert() {
 }
 
 function closeAlert() {
-    // const chk = document.getElementById("chk-hide");
-    // if (chk.checked) {
-    //     const today = new Date().toISOString().split("T")[0];
-    //     localStorage.setItem("hideAlertDate", today);
-    // }
+    const chk = document.getElementById("chk-hide");
+    if (chk.checked) {
+        const today = new Date().toISOString().split("T")[0];
+        localStorage.setItem("hideAlertDate", today);
+    }
     document.getElementById("alert-box").style.display = "none";
 }
